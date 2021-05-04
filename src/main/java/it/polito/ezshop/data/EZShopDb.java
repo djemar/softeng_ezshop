dce85f4c8c364e571db2647d003e7efb112056c9
Diego Marino <marinodiego.96@gmail.com>
2021-05-04 22:45:04 +0200

    db: add insertUser function

--- src/main/java/it/polito/ezshop/data/EZShopDb.java
+++ src/main/java/it/polito/ezshop/data/EZShopDb.java
@@ -31,4 +31,31 @@ public class EZShopDb {
             System.err.println(e.getMessage());
         }
     }
+
+    public void insertUser(UserImpl user) {
+        try {
+            PreparedStatement pstmt =
+                    connection.prepareStatement("insert into users values(?, ?, ?, ?)");
+            pstmt.setQueryTimeout(30); // set timeout to 30 sec.
+            pstmt.setString(1, user.getUsername()); // the index refers to the ? in the statement
+            pstmt.setString(2, user.getPassword());
+            pstmt.setInt(3, user.getId());
+            pstmt.setString(4, user.getRole());
+            pstmt.executeUpdate();
+
+            Statement stmt = connection.createStatement();
+            ResultSet rs;
+            rs = stmt.executeQuery("select * from users");
+
+            while (rs.next()) {
+                // read the result set
+                System.out.println("name = " + rs.getString("username") + ", id = "
+                        + rs.getInt("id") + ", role = " + rs.getString("role"));
+            }
+        } catch (SQLException e) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        }
+    }
 }
