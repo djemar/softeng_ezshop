e52c63f8f7149eb9212cfe5ad02b56636d8144b7
Diego Marino <marinodiego.96@gmail.com>
2021-05-04 22:43:13 +0200

    add EZShopDb class to manage db queries

---
+++ src/main/java/it/polito/ezshop/data/EZShopDb.java
@@ -0,0 +1,34 @@
+package it.polito.ezshop.data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+public class EZShopDb {
+    private Connection connection = null;
+
+    public void createConnection() {
+        try {
+            // create a database connection
+            this.connection = DriverManager.getConnection("jdbc:sqlite:ezshop.db");
+
+        } catch (SQLException e) {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null)
+                connection.close();
+        } catch (SQLException e) {
+            // connection close failed.
+            System.err.println(e.getMessage());
+        }
+    }
+}
