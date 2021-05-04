dd2ed8456c705a27188957a253bdd2d330ac03b5
Diego Marino <marinodiego.96@gmail.com>
2021-05-04 22:45:39 +0200

    db: test method

--- src/main/java/it/polito/ezshop/EZShop.java
+++ src/main/java/it/polito/ezshop/EZShop.java
@@ -1,14 +1,25 @@
 package it.polito.ezshop;
 
+import it.polito.ezshop.data.EZShopDb;
 import it.polito.ezshop.data.EZShopInterface;
+import it.polito.ezshop.data.UserImpl;
 import it.polito.ezshop.view.EZShopGUI;
-
 
 public class EZShop {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         EZShopInterface ezShop = new it.polito.ezshop.data.EZShop();
         EZShopGUI gui = new EZShopGUI(ezShop);
+
+        testDb();
+
+    }
+
+    public static void testDb() { // test function for db implementation
+        EZShopDb ezshopDb = new EZShopDb();
+        ezshopDb.createConnection();
+        ezshopDb.insertUser(new UserImpl("john", "pippo", "tester", 0));
+        ezshopDb.closeConnection();
     }
 
 }
