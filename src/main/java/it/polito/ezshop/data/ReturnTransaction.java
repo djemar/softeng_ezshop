9cbbdc2795b3cad0f5be3300089462f14e00256a
Diego Marino <marinodiego.96@gmail.com>
2021-05-11 00:23:37 +0200

    update ReturnTransaction class

--- src/main/java/it/polito/ezshop/data/ReturnTransaction.java
+++ src/main/java/it/polito/ezshop/data/ReturnTransaction.java
@@ -1,15 +1,25 @@
 package it.polito.ezshop.data;
 
-public class ReturnTransaction{
+import java.util.HashMap;
+
+public class ReturnTransaction {
 
 	private Integer returnId;
+	private Integer transactionId;
 	private String productCode;
 	private int amount;
+	private HashMap<String, Integer> returnedProductsMap;
 
 	public ReturnTransaction(Integer returnId, String productCode, int amount) {
-		this.returnId=returnId;
-		this.productCode=productCode;
-		this.amount=amount;
+		// this should be a useless constructor
+		this.returnId = returnId;
+		this.productCode = productCode;
+		this.amount = amount;
+	}
+
+	public ReturnTransaction(Integer transactionId) {
+		this.transactionId = transactionId;
+		// TODO generate return ID if transactionId is valid
 	}
 
 	public Integer getReturnId() {
@@ -34,6 +44,26 @@ public class ReturnTransaction{
 
 	public void setAmount(int amount) {
 		this.amount = amount;
+	}
+
+	public Integer getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(Integer transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public HashMap<String, Integer> getReturnedProductsMap() {
+		return returnedProductsMap;
+	}
+
+	public void addProductToReturn(String productCode, int amount) {
+		this.returnedProductsMap.put(productCode, amount);
+	}
+
+	public void setReturnedProductsMap(HashMap<String, Integer> returnedProductsMap) {
+		this.returnedProductsMap = returnedProductsMap;
 	}
 
 }
