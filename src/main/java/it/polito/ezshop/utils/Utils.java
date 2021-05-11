5e708fcd14298506d47050b07fe3cf73e3eac2a4
Diego Marino <marinodiego.96@gmail.com>
2021-05-11 13:31:19 +0200

    add utils class

---
+++ src/main/java/it/polito/ezshop/utils/Utils.java
@@ -0,0 +1,39 @@
+package it.polito.ezshop.utils;
+
+public class Utils {
+    public static boolean validateBarcode(String code) {
+        if (code.length() == 13) {
+            int sum = 0;
+            for (int i = 0; i < 12; i++) {
+                if ((i + 1) % 2 == 0)
+                    sum += Character.getNumericValue(code.charAt(i)) * 3;
+                else
+                    sum += Character.getNumericValue(code.charAt(i));
+            }
+            if (Math.round((sum + 5) / 10.0) * 10 - sum == Character
+                    .getNumericValue(code.charAt(12)))
+                return true;
+        }
+
+        return false;
+    }
+
+    static boolean validateCreditCard(String number) {
+        // Luhn algorithm
+        int nDigits = number.length();
+        int nSum = 0;
+        boolean isSecond = false;
+        for (int i = nDigits - 1; i >= 0; i--) {
+            int d = number.charAt(i) - '0';
+            if (isSecond == true)
+                d = d * 2;
+            nSum += d / 10;
+            nSum += d % 10;
+
+            isSecond = !isSecond;
+        }
+        return (nSum % 10 == 0);
+    }
+
+}
+
