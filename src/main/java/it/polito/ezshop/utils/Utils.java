3f722a0ec59b033ae0eed763aff90e16cc5f0b9e
Diego Marino <marinodiego.96@gmail.com>
2021-05-16 21:52:47 +0200

    add update file checks

--- src/main/java/it/polito/ezshop/utils/Utils.java
+++ src/main/java/it/polito/ezshop/utils/Utils.java
@@ -15,60 +15,62 @@ import it.polito.ezshop.data.*;
 
 
 public class Utils {
-    public static boolean validateBarcode(String code) {
+
+	public static boolean validateBarcode(String code) {
 		if (code.length() >= 12 && code.length() <= 14) {
 			int sum = 0;
 			boolean x3 = true;
-			for (int i = code.length()-2; i >= 0; i--) {
+			for (int i = code.length() - 2; i >= 0; i--) {
 				if (x3) {
 					sum += Character.getNumericValue(code.charAt(i)) * 3;
 					x3 = false;
-				}	
-				else {
+				} else {
 					sum += Character.getNumericValue(code.charAt(i));
 					x3 = true;
-				}	
+				}
 			}
-			if (Math.ceil((sum + 5) / 10.0) * 10 - sum == Character.getNumericValue(code.charAt(code.length()-1)))
+			if (Math.ceil((sum + 5) / 10.0) * 10 - sum == Character
+					.getNumericValue(code.charAt(code.length() - 1)))
 				return true;
 		}
-	
+
 		return false;
 
-    }
+	}
 
-    public static boolean isOnlyDigit(String string) {
-        if (string.matches("-?\\d+(\\.\\d+)?"))
-            return true;
-        else
-            return false;
-    }
+	public static boolean isOnlyDigit(String string) {
+		if (string.matches("-?\\d+(\\.\\d+)?"))
+			return true;
+		else
+			return false;
+	}
 
-    public static boolean validateCreditCard(String number) {
-        // Luhn algorithm
-        int nDigits = number.length();
-        int nSum = 0;
-        boolean isSecond = false;
-        for (int i = nDigits - 1; i >= 0; i--) {
-            int d = number.charAt(i) - '0';
-            if (isSecond == true)
-                d = d * 2;
-            nSum += d / 10;
-            nSum += d % 10;
+	public static boolean validateCreditCard(String number) {
+		// Luhn algorithm
+		int nDigits = number.length();
+		int nSum = 0;
+		boolean isSecond = false;
+		for (int i = nDigits - 1; i >= 0; i--) {
+			int d = number.charAt(i) - '0';
+			if (isSecond == true)
+				d = d * 2;
+			nSum += d / 10;
+			nSum += d % 10;
 
-            isSecond = !isSecond;
-        }
-        return (nSum % 10 == 0);
-    }
+			isSecond = !isSecond;
+		}
+		return (nSum % 10 == 0);
+	}
 
-    public static boolean containsProduct(final List<TicketEntry> list, final String productCode) {
-        return list.stream().anyMatch(x-> x.getBarCode().equals(productCode));
-    }
+	public static boolean containsProduct(final List<TicketEntry> list, final String productCode) {
+		return list.stream().anyMatch(x -> x.getBarCode().equals(productCode));
+	}
 
-    public static TicketEntry getProductFromEntries(final List<TicketEntry> list,
-            final String productCode) {
-        return list.stream().filter(o -> o.getBarCode().equals(productCode)).findFirst().get();
-    }
+	public static TicketEntry getProductFromEntries(final List<TicketEntry> list,
+			final String productCode) {
+		return list.stream().filter(o -> o.getBarCode().equals(productCode)).findFirst().get();
+	}
+
 	private static List<String> readData(String file) {
 		try (BufferedReader in = new BufferedReader(new FileReader(file))) {
 			return in.lines().collect(toList());
@@ -77,35 +79,44 @@ public class Utils {
 			return null;
 		}
 	}
+
 	public static boolean fromFile(String creditcard, double total, String file) {
 		List<String> lines = Utils.readData(file);
 		return lines.stream().anyMatch(row -> {
-			if(!row.startsWith("#")) {
+			if (!row.startsWith("#")) {
 				System.out.print(row);
 				String[] cells = row.split(";");
-				return cells[0].equalsIgnoreCase(creditcard) && Double.parseDouble(cells[1]) >= total;
+				return cells[0].equalsIgnoreCase(creditcard)
+						&& Double.parseDouble(cells[1]) >= total;
 
 			}
 			return false;
 		});
 	}
 
-	public static void updateFile(String file, String creditcard, double total){
+	public static boolean updateFile(String file, String creditcard, double total) {
+		boolean found = false;
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(file));
-			String line, newline,content = null;
-			while((line = br.readLine())!= null){
-				if(line.startsWith("#")) {
-					content=content+line+"\n";
+			String line, newline, content = null;
+			while ((line = br.readLine()) != null) {
+				if (line.startsWith("#")) {
+					content = content + line + "\n";
 					continue;
 				}
 				newline = line;
 				String l[] = line.split(";");
-				if(l[0].equalsIgnoreCase(creditcard)) {
+				if (l[0].equalsIgnoreCase(creditcard)) {
+					found = true;
+					System.out.println(creditcard + " " + Double.parseDouble(l[1]));
 					total = Double.parseDouble(l[1]) - total;
-					newline= new String(l[0] + ";"+ String.valueOf(total) );
+					if (total < 0) {
+						System.out.println(total);
+						return false;
+					}
+					newline = new String(l[0] + ";" + String.valueOf(total));
 				}
-				content=content+ newline+ "\n";
+				content = content + newline + "\n";
 			}
 			br.close();
 			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
@@ -113,8 +124,12 @@ public class Utils {
 			bw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
+			return false;
 		}
+		if (!found)
+			return false;
+		return true;
 	}
-	
+
 }
 
