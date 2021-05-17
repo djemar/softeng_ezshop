35bfd094da1bd28fed5b829f41d2c0701171f38c
Diego Marino <marinodiego.96@gmail.com>
2021-05-17 17:25:29 +0200

    test returnTransaction

---
+++ src/test/java/it/polito/ezshop/whiteBoxTests/TestR7_ReturnTransaction.java
@@ -0,0 +1,62 @@
+package it.polito.ezshop.whiteBoxTests;
+
+import org.junit.Test;
+import it.polito.ezshop.data.ReturnTransaction;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import java.util.HashMap;
+import org.junit.Before;
+
+public class TestR7_ReturnTransaction {
+    ReturnTransaction returnTransaction;
+    HashMap<String, Integer> returnedProductsMap = new HashMap<String, Integer>();
+
+    @Before
+    public void setup() {
+        returnTransaction = new ReturnTransaction(1, 5, "OPEN", 20);
+        returnedProductsMap.put("65164684113337", 2);
+        returnedProductsMap.put("2905911158926", 7);
+    }
+
+    @Test
+    public void testSetGetReturnId() {
+        Integer id = 1;
+        assertEquals(id, returnTransaction.getReturnId());
+        id = 2;
+        returnTransaction.setReturnId(id);
+        assertEquals(id, returnTransaction.getReturnId());
+    }
+
+    @Test
+    public void testSetGetTransactionId() {
+        Integer id = 5;
+        assertEquals(id, returnTransaction.getTransactionId());
+        id = 10;
+        returnTransaction.setTransactionId(id);
+        assertEquals(id, returnTransaction.getTransactionId());
+    }
+
+    @Test
+    public void testSetGetStatus() {
+        String status = "OPEN";
+        assertEquals(status, returnTransaction.getStatus());
+        status = "CLOSED";
+        returnTransaction.setStatus(status);
+        assertEquals(status, returnTransaction.getStatus());
+    }
+
+    @Test
+    public void testSetGetTotal() {
+        double total = 20;
+        assertEquals(total, returnTransaction.getTotal(), 0.001);
+        total = 10;
+        returnTransaction.setTotal(total);
+        assertEquals(total, returnTransaction.getTotal(), 0.001);
+    }
+
+    @Test
+    public void testSetGetReturnedMap() {
+        returnTransaction.setReturnedProductsMap(returnedProductsMap);
+        assertEquals(returnedProductsMap, returnTransaction.getReturnedProductsMap());
+    }
+}
