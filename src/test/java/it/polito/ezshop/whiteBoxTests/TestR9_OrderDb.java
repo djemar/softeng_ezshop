24f23f83d8e7cc0e23699ff74bfc9b6d6bdc25b7
elitedde <79220600+elitedde@users.noreply.github.com>
2021-05-18 13:24:22 +0200

    added TestR9_OrderDb

---
+++ src/test/java/it/polito/ezshop/whiteBoxTests/TestR9_OrderDb.java
@@ -0,0 +1,26 @@
+package it.polito.ezshop.whiteBoxTests;
+import org.junit.Test;
+
+import it.polito.ezshop.data.EZShopDb;
+import it.polito.ezshop.data.OrderImpl;
+import it.polito.ezshop.data.SaleTransactionImpl;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+
+public class TestR9_OrderDb {
+	EZShopDb ezshopDb = new EZShopDb();
+		  
+		  @Test
+		  public void testUpdateOrd() {
+		    ezshopDb.createConnection();
+			ezshopDb.resetTables();		    
+			Integer id = ezshopDb.insertOrder(new OrderImpl("56789342",10.5, 50));
+			assertTrue(ezshopDb.updateOrder(id, "PAYED", 5));
+			ezshopDb.closeConnection();
+		  }
+}
+
