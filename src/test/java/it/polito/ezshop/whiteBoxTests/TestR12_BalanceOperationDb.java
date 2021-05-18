61b084a89f745ccd33325fe841c6eb46e7ee72b1
Diego Marino <marinodiego.96@gmail.com>
2021-05-18 16:44:08 +0200

    add db test balanceOperation

--- src/test/java/it/polito/ezshop/whiteBoxTests/TestR12_BalanceOperationDb.java
+++ src/test/java/it/polito/ezshop/whiteBoxTests/TestR12_BalanceOperationDb.java
@@ -1,5 +1,44 @@
 package it.polito.ezshop.whiteBoxTests;
 
-public class TestR12_BalanceOperationDb {
+import org.junit.Test;
+import it.polito.ezshop.data.EZShopDb;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.After;
+import org.junit.Before;
 
+public class TestR12_BalanceOperationDb {
+    EZShopDb ezshopDb = new EZShopDb();
+
+    @Before
+    public void setup() {
+        ezshopDb.createConnection();
+        ezshopDb.resetDB();
+        ezshopDb.closeConnection();
+    }
+
+    @After
+    public void clean() {
+        ezshopDb.createConnection();
+        ezshopDb.resetDB();
+        ezshopDb.closeConnection();
+    }
+
+    @Test
+    public void testValidRecordBalanceUpdate() {
+        boolean test;
+        ezshopDb.createConnection();
+        test = ezshopDb.recordBalanceUpdate(10d);
+        assertTrue(test);
+        ezshopDb.closeConnection();
+    }
+
+    @Test
+    public void testInvalidRecordBalanceUpdate() {
+        boolean test;
+        // ezshopDb.createConnection();
+        test = ezshopDb.recordBalanceUpdate(10d);
+        assertFalse(test);
+        // ezshopDb.closeConnection();
+    }
 }
